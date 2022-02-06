a4cc2610a60a6c3497601e3d259b5d2261793ce1
jonjjon <poptanza@g.skku.edu>
2022-02-07 00:14:42 +0900

    minimal changes in sample text for testing git commit and push function

--- app/src/main/java/com/proj/newer_daang/BookmarkActivity.kt
+++ app/src/main/java/com/proj/newer_daang/BookmarkActivity.kt
@@ -45,9 +45,9 @@ class BookmarkActivity : AppCompatActivity() {
         })
 
         bookmarks.apply {
-            add(BookmarkData( name = "용어북마크1", meaning = "용어북마크1 용어북마크1 용어북마크1 용어북마크1용어북마크1 용어북마크1용어북마크1 용어북마크1.", category = "IT/과학", tag1 = "#취미생활 #원예생활 #플랜트", tag2 = "해시2"))
-            add(BookmarkData( name = "term_sample_length_check", meaning = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum a dolor luctus, fringilla est sed, volutpat eros. Vivamus tristique gravida.", category = "정치", tag1 = "해시샘플", tag2 = "해시또"))
-            add(BookmarkData( name = "term_sample", meaning = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum a dolor luctus.", category = "IT/과학", tag1 = "해시", tag2 = "해시2"))
+            add(BookmarkData( name = "용어북마크1", meaning = "근로자는 근로조건의 향상을 위하여 자주적인 단결권·단체교섭권 및 단체행동권을 가진다. ", category = "IT/과학", tag1 = "#취미생활 #원예생활 #플랜트", tag2 = "해시2"))
+            add(BookmarkData( name = "용어북마크2입니다", meaning = "이 헌법공포 당시의 국회의원의 임기는 제1항에 의한 국회의 최초의 집회일 전일까지로 한다.", category = "정치", tag1 = "해시샘플", tag2 = "해시또"))
+            add(BookmarkData( name = "용어북마크3", meaning = "광물 기타 중요한 지하자원·수산자원·수력과 경제상 이용할 수 있는 자연력은 법률이 정하는 바에 의하여 일정한 기간 그 채취·개발 또는 이용을 특허할 수 있다.", category = "IT/과학", tag1 = "해시", tag2 = "해시2"))
             add(BookmarkData( name = "term_sample_length_check", meaning = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum a dolor luctus, fringilla est sed, volutpat eros. Vivamus tristique gravida.", category = "정치", tag1 = "해시샘플", tag2 = "해시또"))
             add(BookmarkData( name = "term_sample", meaning = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum a dolor luctus.", category = "IT/과학", tag1 = "해시", tag2 = "해시2"))
             add(BookmarkData( name = "term_sample_length_check", meaning = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum a dolor luctus, fringilla est sed, volutpat eros. Vivamus tristique gravida.", category = "정치", tag1 = "해시샘플", tag2 = "해시또"))
